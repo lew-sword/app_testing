46d800629c408c6e9dcb49029a6a17d88aec8851
Lewis Sword (lew-sword) <github@lew-sword.com>
2025-02-21 12:20:29 +0000

    alpha app includes stake and alpha_stake fields

--- alpha_stake_app/alpha_stake_app.py
+++ alpha_stake_app/alpha_stake_app.py
@@ -1,3 +1,4 @@
+from typing import List
 import streamlit as st
 import bittensor as bt
 import pandas as pd
@@ -58,39 +59,46 @@ class SubnetAnalyzer:
         if self.metagraph is None:
             raise ValueError(f"Subnet {self.subnet} does not exist.")
         
-    def get_macro_vali_key(self):
+    def get_macro_vali_key(self)->List[str]:
         if self.subnet == 1:
-            return "5FRXwb2qsEhqDQQKcm5m2MF26xTWwW65MHTEtKFFydypuqjG"
+            return ["5FRXwb2qsEhqDQQKcm5m2MF26xTWwW65MHTEtKFFydypuqjG"]
         elif self.subnet == 9:
-            return "5FRXwb2qsEhqDQQKcm5m2MF26xTWwW65MHTEtKFFydypuqjG" #"5HBtpwxuGNL1gwzwomwR7sjwUt8WXYSuWcLYN6f9KpTZkP4k"
+            return ["5FRXwb2qsEhqDQQKcm5m2MF26xTWwW65MHTEtKFFydypuqjG", "5HBtpwxuGNL1gwzwomwR7sjwUt8WXYSuWcLYN6f9KpTZkP4k"]
         elif self.subnet == 13:
-            return "5FRXwb2qsEhqDQQKcm5m2MF26xTWwW65MHTEtKFFydypuqjG"
+            return ["5FRXwb2qsEhqDQQKcm5m2MF26xTWwW65MHTEtKFFydypuqjG"]
         elif self.subnet == 25:
-            return "5FRXwb2qsEhqDQQKcm5m2MF26xTWwW65MHTEtKFFydypuqjG"
+            return ["5FRXwb2qsEhqDQQKcm5m2MF26xTWwW65MHTEtKFFydypuqjG"]
         elif self.subnet == 37:
-            return "5HQWSRrS4sjxwgf9EQKG7arRHE2WqMTcMap2QZJWsvQFXfQK"
+            return ["5HQWSRrS4sjxwgf9EQKG7arRHE2WqMTcMap2QZJWsvQFXfQK"]
         else:
-            return"non_macrocosmos_key"
+            return ["non_macrocosmos_key"]
 
     def get_active_uids(self):
         """Return active UIDs and their corresponding coldkeys."""
         active_uids = [i for i in range(len(self.metagraph.active)) if self.metagraph.active[i]]
         return active_uids
 
-    def get_total_alpha_by_uid(self, uid: int):
+    def get_alpha_data_by_uid(self, uid: int):
         """Get the total staked alpha for a given active UID."""
         if uid >= len(self.metagraph.hotkeys):
             print(f"UID {uid} is out of range for subnet {self.metagraph.netuid}")
             return None
 
         staked_alpha = self.metagraph.alpha_stake[uid]
-        return uid, staked_alpha
+        stake = self.metagraph.stake[uid]
+        return uid, staked_alpha, stake
 
     def get_stake_data(self):
         """Fetch stake data for all active UIDs in the subnet."""
         uid_stake_dict = {}
+        macro_vali_keys = self.get_macro_vali_key()
         for uid in self.get_active_uids():
-            uid_stake_dict[uid] = {"stake": self.get_total_alpha_by_uid(uid)[1], "coldkey": self.metagraph.coldkeys[uid], "v_permit": self.metagraph.validator_permit[uid], "macro_vali": 1 if self.metagraph.coldkeys[uid]==self.get_macro_vali_key() else 0}
+            stake_data_for_uid = self.get_alpha_data_by_uid(uid)
+            uid_stake_dict[uid] = {"alpha_stake": stake_data_for_uid[1], #gets staked alpha
+                                   "stake": stake_data_for_uid[2], #gets total stake like "stake weight" on taostats
+                                   "coldkey": self.metagraph.coldkeys[uid],
+                                   "v_permit": self.metagraph.validator_permit[uid], 
+                                   "macro_vali": 1 if self.metagraph.coldkeys[uid] in macro_vali_keys else 0}
         return uid_stake_dict
 
     def to_dataframe(self):
@@ -102,7 +110,7 @@ class SubnetAnalyzer:
         # Reset index to make UID a column
         df.reset_index(inplace=True)
         # Rename index column to 'UID'
-        df.rename(columns={"index": "UID", "stake": "Stake", "coldkey": "Coldkey", "v_permit": "V_Permit", "macro_vali": "Macro Vali" }, inplace=True)
+        df.rename(columns={"index": "UID", "alpha_stake": "Alpha_Stake", "stake": "Stake" , "coldkey": "Coldkey", "v_permit": "V_Permit", "macro_vali": "Macro_Vali" }, inplace=True)
         
         # Sort by 'Stake' in descending order
         df = df.sort_values(by="Stake", ascending=False)
@@ -112,7 +120,7 @@ class SubnetAnalyzer:
     def display_stake_data(self):
         """Display stake data in Streamlit."""
         df = self.to_dataframe()
-        st.write(f"### Stake data for subnet {self.subnet} at block `{self.current_block}`")
+        st.write(f"### Alpha stake data for subnet `{self.subnet}` at block `{self.current_block}`")
         st.dataframe(df.set_index("UID"), use_container_width=True)
 
 
@@ -128,26 +136,33 @@ inst_sn25 = SubnetAnalyzer(subnet=25, subtensor=subtensor, block=current_block)
 inst_sn37 = SubnetAnalyzer(subnet=37, subtensor=subtensor, block=current_block)
 
 
-def make_chart(df, color_by_v_permit=False, color_by_macro_vali=False):
+def make_chart(df, color_by_v_permit=False, color_by_macro_vali_as=False, color_by_macro_vali_s=False):
     """Create a pie chart with colors based on either UID or V_Permit."""
-    
+    theta_param = "Alpha_Stake"
     if color_by_v_permit:
         color_scheme = alt.Scale(domain=[True, False], range=["#4CAF50", "#FF5733"])
         color_encoding = alt.Color("V_Permit:N", scale=color_scheme, title="Validator Permit")
-    elif color_by_macro_vali:
-        # Gold for Macro Validator, Blue for Others
+
+    elif color_by_macro_vali_as:
+        # Gold for Macro Validator, Blue for Others on alpha_stake
         color_scheme = alt.Scale(domain=[1, 0], range=["#FFD700", "#4682B4"])
-        color_encoding = alt.Color("Macro Vali:O", scale=color_scheme, title="Macro Validator")
-        
+        color_encoding = alt.Color("Macro_Vali:O", scale=color_scheme, title="Macro Validator")
+
+    elif color_by_macro_vali_s:
+        # Gold for Macro Validator, Blue for Others on stake
+        theta_param="Stake"
+        color_scheme = alt.Scale(domain=[1, 0], range=["#FFD700", "#4682B4"])
+        color_encoding = alt.Color("Macro_Vali:O", scale=color_scheme, title="Macro Validator")
+
     else:
         color_scheme = alt.Scale(scheme="dark2")  # Alternative color scheme for UID
         color_encoding = alt.Color("UID:N", scale=color_scheme, title="UID")
     
     # Create pie chart
     return alt.Chart(df).mark_arc().encode(
-        theta="Stake",
+        theta=f"{theta_param}:Q",
         color=color_encoding,
-        tooltip=["UID", "Stake", "V_Permit", "Coldkey"]
+        tooltip=["UID", "Stake", "Alpha_Stake", "V_Permit", "Coldkey", "Macro_Vali"]
     ).properties(
         title="UID Staked Alphas Distribution",
         width=500,
@@ -157,7 +172,8 @@ def make_chart(df, color_by_v_permit=False, color_by_macro_vali=False):
 
 # Display in Streamlit
 color_by_v_permit = st.checkbox("Colour by Validator Permit?", value=False)
-color_by_macro_vali = st.checkbox("Colour by Macro Validator Vs. Others?", value=False)
+color_by_macro_vali_as = st.checkbox("Colour by Macro Validator Vs. Others on Alpha_Stake?", value=False)
+color_by_macro_vali_s = st.checkbox("Colour by Macro Validator Vs. Others on Stake?", value=False)
 # Create separate rows for each subnet
 for inst in [inst_sn1, inst_sn9, inst_sn13, inst_sn25, inst_sn37]:
     col1, col2 = st.columns([1, 1])  # Equal width columns
@@ -166,7 +182,7 @@ for inst in [inst_sn1, inst_sn9, inst_sn13, inst_sn25, inst_sn37]:
         inst.display_stake_data()  # Show the dataframe
 
     with col2:
-        st.altair_chart(make_chart(inst.to_dataframe(), color_by_v_permit, color_by_macro_vali), use_container_width=True)  # Show the chart
+        st.altair_chart(make_chart(inst.to_dataframe(), color_by_v_permit, color_by_macro_vali_as,color_by_macro_vali_s), use_container_width=True)  # Show the chart
 
 # Sleep and rerun settings
 wait_message = st.empty()
